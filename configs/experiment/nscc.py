b338389c738e80b328960f0f5c840a01350e919e
wengkit1 <soowengkit.00@gmail.com>
2025-07-01 10:13:38 +0800

    missed out output dir

--- configs/experiment/nscc.py
+++ configs/experiment/nscc.py
@@ -1,6 +1,7 @@
 """NSCC experiment configuration"""
 from configs.config_schema import ExperimentConfig
-
+import os
+scratch_dir = os.getenv("SCRATCH_DIR", "/scratch/users/nus/e0686150")
 nscc_experiment = ExperimentConfig(
     wandb_project="peer-gemma-7b-nscc",
     experiment_name="gemma_7b_peer",
@@ -8,6 +9,6 @@ nscc_experiment = ExperimentConfig(
     wandb_notes="Training PEER Gemma 7B on NSCC ASPIRE 2A",
     log_every_n_steps=50,
     save_top_k=3,
-    output_dir="/scratch/$USER/peer_gemma_experiments",  # Use scratch space
+    output_dir=f"{scratch_dir}/peer_gemma_experiments",  # Use the actual scratch dir
     seed=42,
 )
