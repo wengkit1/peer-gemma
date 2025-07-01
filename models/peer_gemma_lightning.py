27b18746467fefc35ba87693f578197bee7ef912
wengkit1 <soowengkit.00@gmail.com>
2025-07-01 13:39:01 +0800

    missed out import error

--- models/peer_gemma_lightning.py
+++ models/peer_gemma_lightning.py
@@ -16,7 +16,7 @@ import os
 
 import sys
 sys.path.append('.')
-from peer_gemma import PEERGemmaForCausalLM, create_custom_gemma_config
+from peer_gemma import PEERGemmaForCausalLM
 
 
 class PEERGemmaLightningModule(pl.LightningModule):
