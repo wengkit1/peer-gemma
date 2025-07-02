de279ef103fad47d550bba6c81019f3caeedfc29
wengkit1 <soowengkit.00@gmail.com>
2025-07-02 12:48:07 +0800

    less greedy

--- configs/system/nscc.py
+++ configs/system/nscc.py
@@ -3,7 +3,7 @@ from configs.config_schema import SystemConfig
 
 nscc_system = SystemConfig(
     accelerator="gpu",
-    devices=6,
+    devices=4,
     num_workers=8,
     pin_memory=True,
     persistent_workers=True,
